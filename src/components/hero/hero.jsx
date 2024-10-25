8da304d83884543ba584e8f6e64759f3164c1b7c
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2024-10-26 02:00:47 +0530

    Initial commit

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -9,7 +9,7 @@ const hero = () => {
          <h1><span>I'm Gajendra Kumar Arya,</span> an aspiring frontend developer.</h1>
          <p>I am a frontend developer, currently a second year student at UPES.</p>
          <div className="hero-action">
-            <div className="hero-connect"><a className='anchor-link' offset={50} href="#Contact">Connect with me</a></div>
+            <div className="hero-connect"><a href="#Contact">Connect with me</a></div>
             <div className="hero-resume">My resume</div>
          </div>
        </div>
