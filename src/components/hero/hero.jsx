da927bbd85cbcebb72ca58af3c69b1ed46279b94
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-04-04 23:43:09 +0530

    Added New profile

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './hero.css'
 import profile_img from '../../assests/gka.neww.jpg'
+import profilePic from '../../assests/profile1.jpg'
 
 const hero = () => {
     return(
        <div id="home" className="hero">
-         <img src={profile_img} alt="" id="img"/>
+         <img src={profilePic} alt="" id="img"/>
          <h1><span>I'm Gajendra Kumar Arya,</span> an aspiring frontend developer.</h1>
          <p>I am a frontend developer, currently a second year student at UPES.</p>
          <div className="hero-action">
