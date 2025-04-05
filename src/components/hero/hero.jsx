fa4e7a3796e37212cb7f915c4affe66d8237857e
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-04-05 11:25:54 +0530

    changes made

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -7,8 +7,8 @@ const hero = () => {
     return(
        <div id="home" className="hero">
          <img src={profilePic} alt="" id="img"/>
-         <h1><span>I'm Gajendra Kumar Arya,</span> an aspiring frontend developer.</h1>
-         <p>I am a frontend developer, currently a second year student at UPES.</p>
+         <h1><span>I'm Gajendra Kumar Arya,</span> an aspiring fullstack developer.</h1>
+         <p>I am a fullstack developer, currently a second year student at UPES.</p>
          <div className="hero-action">
             <div className="hero-connect"><a href="#Contact">Connect with me</a></div>
             <div className="hero-resume">My resume</div>
